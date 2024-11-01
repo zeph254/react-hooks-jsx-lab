c0e1eb059da5bdf793ca8e34f711ec5415876991
HP <ularezephaniah@gmail.com>
2024-11-01 11:37:31 +0300

    JSX

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return<div id="home">
+  <h1>
+    Liza is a Web Developer from New York
+  </h1>
+</div>
+
 }
 
 export default Home;
